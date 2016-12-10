e7d91424d8193edc96367fd8c7615ba73c26d6b2
Nick Klepinger <bodiddlie@gmail.com>
2016-12-10 14:52:36 -0700

    cleaned up code a bit

--- src/lorem-list.jsx
+++ src/lorem-list.jsx
@@ -1,36 +1,30 @@
-import React, {Component, PropTypes} from 'react';
+import React from 'react';
 
 const styles = [
-    { backgroundColor: 'PapayaWhip' },
-    { backgroundColor: 'Orchid' },
-    { backgroundColor: 'DodgerBlue' },
-    { backgroundColor: 'FireBrick' },
-    { backgroundColor: 'Goldenrod' },
+  { backgroundColor: 'PapayaWhip' },
+  { backgroundColor: 'Orchid' },
+  { backgroundColor: 'DodgerBlue' },
+  { backgroundColor: 'FireBrick' },
+  { backgroundColor: 'Goldenrod' },
 ];
 
-export class LoremList extends Component {
-    static propTypes = {
-        paragraphCount: PropTypes.number.isRequired,
-    };
-
-    render() {
-        let lorems = [];
-        for (let i = 0; i < this.props.paragraphCount; i++) {
-            lorems.push(<LoremItem key={i} number={i+1} color={styles[i % 5]} />);
-        }
-        return (
-            <div>
-                <div>
-                {lorems}
-                </div>
-            </div>
-        )
-    }
+export const LoremList = ({paragraphCount}) => {
+  let lorems = [];
+  for (let i = 0; i < paragraphCount; i++) {
+    lorems.push(<LoremItem key={i} number={i+1} color={styles[i % 5]} />);
+  }
+  return (
+    <div>
+      <div>
+        {lorems}
+      </div>
+    </div>
+  )
 }
 
 export const LoremItem = ({number, color}) => (
-    <div>
-        <h3>Item #{number}</h3>
-        <p style={color}>Here is some text.</p>
-    </div>
+  <div>
+    <h3>Item #{number}</h3>
+    <p style={color}>Lorem ipsum dolar sit amet.</p>
+  </div>
 );
